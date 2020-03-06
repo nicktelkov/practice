687a20c1f4a7f751c0ea1b6e0bcdaafcdff9e4a8
nicktelkov <61237298+nicktelkov@users.noreply.github.com>
2020-03-06 18:17:35 +0300

    Update Main.java

--- HW05/src/Main.java
+++ HW05/src/Main.java
@@ -14,21 +14,20 @@ persArray[4] = new Person(...);
 
  */
 public class Main {
-    private static void main(String[] args) {
+    public static void main(String[] args) {
 
         Employee[] employees = new Employee[5];
-        employees[0] = new Employee("Иван", "Иванович", "Иванов", "инженер", "ivan@workmail.com", 892312312, 750000.00 ,27);
-        employees[1] = new Employee("Николай", "Иванович", "Иванов", "инженер", "ivan@workmail.com", 892312312, 750000.00 ,35);
-        employees[2] = new Employee("Петр", "Иванович", "Иванов", "старший инженер", "ivan@workmail.com", 892312312, 750000.00 ,47);
-        employees[3] = new Employee("Дмитрий", "Иванович", "Иванов", "стажер", "ivan@workmail.com", 892312312, 750000.00 ,23);
-        employees[4] = new Employee("Василий", "Иванович", "Иванов", "старший инженер", "ivan@workmail.com", 892312312, 750000.00 ,41);
+        employees[0] = new Employee("Иван", "Иванович", "Иванов", "инженер", "ivan@workmail.com", 892312312, 750000.00, 27);
+        employees[1] = new Employee("Николай", "Иванович", "Иванов", "инженер", "ivan@workmail.com", 892312312, 750000.00, 35);
+        employees[2] = new Employee("Петр", "Иванович", "Иванов", "старший инженер", "ivan@workmail.com", 892312312, 750000.00, 47);
+        employees[3] = new Employee("Дмитрий", "Иванович", "Иванов", "стажер", "ivan@workmail.com", 892312312, 750000.00, 23);
+        employees[4] = new Employee("Василий", "Иванович", "Иванов", "старший инженер", "ivan@workmail.com", 892312312, 750000.00, 41);
 
-      for (int i = 0; i <= employees.length; i ++) {
+      for (int i = 0; i < employees.length; i ++) {
           if (employees[i].getAge() > 40){
               employees[i].employeeInfo();
           }
       }
-
-
     }
+}
 }
