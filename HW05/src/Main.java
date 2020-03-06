5aefba9f93e47c8936824ce1b9b1da865dc20ef7
nicktelkov <61237298+nicktelkov@users.noreply.github.com>
2020-03-06 14:45:06 +0300

    Home Work 05
    Не могу локализовать причину ошибок. Не понимаю: о каком символе идет речь.
    Ошибки:
    
    Main.java:19: error: cannot find symbol
            Employee[] employees = new Employee[5];
            ^
      symbol:   class Employee
      location: class Main
    Main.java:19: error: cannot find symbol
            Employee[] employees = new Employee[5];
                                       ^
      symbol:   class Employee
      location: class Main
    Main.java:20: error: cannot find symbol
            employees[0] = new Employee("Иван", "Иванович", "Иванов", "инженер", "ivan@workmail.com", 892312312, 750000,27);
                               ^
      symbol:   class Employee
      location: class Main
    Main.java:21: error: cannot find symbol
            employees[1] = new Employee("Иван", "Иванович", "Иванов", "инженер", "ivan@workmail.com", 892312312, 750000,35);
                               ^
      symbol:   class Employee
      location: class Main
    Main.java:22: error: cannot find symbol
            employees[2] = new Employee("Иван", "Иванович", "Иванов", "инженер", "ivan@workmail.com", 892312312, 750000,47);
                               ^
      symbol:   class Employee
      location: class Main
    Main.java:23: error: cannot find symbol
            employees[3] = new Employee("Иван", "Иванович", "Иванов", "инженер", "ivan@workmail.com", 892312312, 750000,23);
                               ^
      symbol:   class Employee
      location: class Main
    Main.java:24: error: cannot find symbol
            employees[4] = new Employee("Иван", "Иванович", "Иванов", "инженер", "ivan@workmail.com", 892312312, 750000,41);
                               ^
      symbol:   class Employee
      location: class Main
    7 errors
    error: compilation failed

---
+++ HW05/src/Main.java
@@ -0,0 +1,34 @@
+/*
+Домашнее задание
+Создать класс "Сотрудник" с полями: ФИО, должность, email, телефон, зарплата, возраст.
+Конструктор класса должен заполнять эти поля при создании объекта.
+Внутри класса «Сотрудник» написать метод, который выводит информацию об объекте в консоль.
+Создать массив из 5 сотрудников.
+Пример:
+Person[] persArray = new Person[5]; // Вначале объявляем массив объектов
+persArray[0] = new Person("Ivanov Ivan", "Engineer", "ivivan@mailbox.com", "892312312", 30000, 30); // потом для каждой ячейки массива задаем объект
+persArray[1] = new Person(...);
+...
+persArray[4] = new Person(...);
+С помощью цикла вывести информацию только о сотрудниках старше 40 лет.
+
+ */
+public class Main {
+    private static void main(String[] args) {
+
+        Employee[] employees = new Employee[5];
+        employees[0] = new Employee("Иван", "Иванович", "Иванов", "инженер", "ivan@workmail.com", 892312312, 750000,27);
+        employees[1] = new Employee("Иван", "Иванович", "Иванов", "инженер", "ivan@workmail.com", 892312312, 750000,35);
+        employees[2] = new Employee("Иван", "Иванович", "Иванов", "старший инженер", "ivan@workmail.com", 892312312, 750000,47);
+        employees[3] = new Employee("Иван", "Иванович", "Иванов", "стажер", "ivan@workmail.com", 892312312, 750000,23);
+        employees[4] = new Employee("Иван", "Иванович", "Иванов", "старший инженер", "ivan@workmail.com", 892312312, 750000,41);
+
+      for (int i = 0; i <= employees.length; i ++) {
+          if (employees[i].getAge() > 40){
+              employees[i].employeeInfo();
+          }
+      }
+
+
+    }
+}
